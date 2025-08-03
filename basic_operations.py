9c37962be254fd19e9130c8a510bc4a451e3936f
Roger Gaitu <rogergaitu@gmail.com>
2025-08-03 20:25:20 +0000

    basic_operations.py has been added

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum_total = number1 + number2
+sub_total = number1 - number2
+pro_total = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum_total}")
+print(f"Subtraction of {number1} and {number2} is {sub_total}")
+print(f"Product of {number1} and {number2} is {pro_total}")
+
