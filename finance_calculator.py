206bd57cd2f5b50f524c26e14f2c57a885e24151
Roger Gaitu <rogergaitu@gmail.com>
2025-08-05 23:40:04 +0000

    finance_calculator.py file has been added

---
+++ finance_calculator.py
@@ -0,0 +1,8 @@
+income = input("Enter your monthly income")
+expenses = input("Enter your total monthly expenses")
+savings = income - expenses
+
+monthly_savings = (f"Your monthly savings are:{savings}")
+
+projected_savings = (savings * 12 + (savings * 12 * 0.05))
+
